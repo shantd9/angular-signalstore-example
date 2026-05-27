8c2c1031595e2f0701557833865971f2b60a808c
Shant Derounian <shantderounian@gmail.com>
2026-05-27 07:37:08 +0200

    implement loading of todos in TodosStore and display in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,22 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosStore } from './store/todos.store';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-
   store = inject(TodosStore);
 
   ngOnInit() {
-    this.store.todos()
+    this.loadTodos().then(() => console.log('todos loaded'));
+  }
+
+  async loadTodos() {
+    await this.store.loadAll();
   }
 }
--- src/app/store/todos.store.ts
+++ src/app/store/todos.store.ts
@@ -1,5 +1,7 @@
 import { Todo } from '../model/todo.model';
-import { signalStore, withState } from '@ngrx/signals';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { TodosService } from '../services/todos.service';
+import { inject } from '@angular/core';
 
 export type TodosFilter = "all" | "completed" | "pending";
 
@@ -15,4 +17,14 @@ const initialState: TodosState = {
   filter: "all",
 }
 
-export const TodosStore = signalStore({providedIn: 'root'}, withState(initialState))
+export const TodosStore = signalStore({providedIn: 'root'}, withState(initialState), withMethods(
+  (store, todosService = inject(TodosService)) => ({
+    async loadAll() {
+      patchState(store, { loading: true });
+
+      const todos = await todosService.getTodos()
+
+      patchState(store, { todos: todos, loading: false });
+    }
+  })
+))
